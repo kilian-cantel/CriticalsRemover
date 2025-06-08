b4b4193428d85cb7c27bca5fda0960d78105436d
kilian-cantel <lemaitre722.contact@gmail.com>
2025-06-08 16:00:40 +0200

    Generalize the plugin to all entities.

--- src/main/java/com/atzer/criticalsRemover/OnEntityTakeDamage.java
+++ src/main/java/com/atzer/criticalsRemover/OnEntityTakeDamage.java
@@ -7,21 +7,21 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 
 public class OnEntityTakeDamage implements Listener {
 
     @EventHandler
     public boolean onEntityTakeDamage(@NotNull EntityDamageByEntityEvent event) {
-        if (!(event.getEntity() instanceof Player player)) return false;
-        player = player.getKiller();
-
-        if (player == null) return false;
+        if (!(event.getDamager() instanceof Player player)) return false;
 
         @SuppressWarnings("deprecation")
         boolean flag = player.getFallDistance() > 0.0F && !player.isOnGround() && !player.hasPotionEffect(PotionEffectType.BLINDNESS) && player.getVehicle() == null;
 
         if (flag) {
-            player.damage(event.getDamage() / 1.5F);
+            event.setCancelled(true);
+            Objects.requireNonNull(event.getEntity().getLastDamageCause()).setDamage(event.getDamage() / 1.5F);
         }
         return true;
     }
